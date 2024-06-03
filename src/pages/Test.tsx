20f91b778b29bec222cb2059cc39d3d719a4893e
duc2807 <nguyentrungduc2807@gmail.com>
2024-06-04 02:43:43 +0700

    build[src/coponnents]: Config shadcnUI library

---
+++ src/pages/Test.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@radix-ui/react-label";
+import { Input } from "@/components/ui/input";
+const Test = () => {
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+        <CardTitle className="font-black text-23.04px">Đăng ký</CardTitle>
+          <CardDescription>
+            Enter your email below to create your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+        <div className="grid gap-1 pb-2">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="email" placeholder="m@example.com" />
+        </div>
+        <div className="grid gap-1 pb-2">
+          <Label htmlFor="password">Password</Label>
+          <Input id="password" type="password" />
+        </div>
+        </CardContent>
+        <CardFooter>
+          <Button>Click me</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default Test;
