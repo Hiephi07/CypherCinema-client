2fb4dad0a1619e92e375fcf560003349be10d699
Nguyen Thien <nguyenvanthien24032004@gmail.com>
2024-06-05 19:33:31 +0700

    feat[Movie_is_show and Sort_movies] - giao dien

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.scss'
 import Test from './pages/Test'
 import './assets/fonts/fonts.scss'
+import Movie_is_showing from './pages/client/Movie_is_show/Movie_is_showing'
+import Phimsapchieu from './pages/client/Sort movies/Sort_movies'
 function App() {
 
   return (
     <>
-      <h4>Hello world</h4>
+    <Movie_is_showing/>
+    <Phimsapchieu/>
       <Test></Test>
     </>
   )
