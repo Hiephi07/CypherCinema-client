eb24c74455231808d968d74f179555c440b56f5f
Nguyen Thien <nguyenvanthien24032004@gmail.com>
2024-06-06 19:07:54 +0700

    feact:[src/pages/client/Event,Promotion]:trang Event và Promotion

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,20 @@ import Test from './pages/Test'
 import './assets/fonts/fonts.scss'
 import Movie_is_showing from './pages/client/Movie_is_show/Movie_is_showing'
 import Phimsapchieu from './pages/client/Sort movies/Sort_movies'
+import Banner from './pages/client/Banner/Banner'
+import Promotion from './pages/client/Promotion/Promotion'
+
+import Event from './pages/client/Event/Event'
+
 function App() {
 
   return (
     <>
     <Movie_is_showing/>
     <Phimsapchieu/>
-      <Test></Test>
+   <Banner/>
+   <Promotion/>
+    <Event/>
     </>
   )
 }
---
+++ src/pages/client/Promotion/Promotion.tsx
@@ -0,0 +1,65 @@
+
+import { Button } from "@/components/ui/button";
+import React, { Component } from "react";
+import Slider from "react-slick";
+import "./Promotion.scss"
+
+function SampleNextArrow(props: { className: any; style: any; onClick: any; }) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "red" }}
+        onClick={onClick}
+      />
+    );
+  }
+  
+  function SamplePrevArrow(props: { className: any; style: any; onClick: any; }) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "green"}}
+        onClick={onClick}
+      />
+    );
+  }
+  
+const Promotion = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        nextArrow: <SampleNextArrow className={undefined} style={undefined} onClick={undefined} />,
+        prevArrow: <SamplePrevArrow className={undefined} style={undefined} onClick={undefined} />
+      };
+      return (
+       <>
+        <h1 className="tieude">Khuyến Mại </h1>
+          <div className="slider-container">
+            <Slider {...settings}>
+              {[...Array(4)].map((_, index) => (
+                <div key={index} className="carl ">
+              
+                    <img src={`src/assets/images/PromotionalEvents/Promotional/item${index + 1}.png`} alt="" className="h-48 object-cover"  />
+               
+                  
+                  <div className="content-nd">
+                   
+                    <div className="nd-promotion">
+                      <b>Chạm thẻ visa – Tặng ngay bắp lớn</b>
+                      <p>Thời gian khuyến mãi: 26/8/2023 – 26/9/2023</p>
+                    </div>
+                   
+                  </div>
+                </div>
+              ))}
+            </Slider>
+          </div>
+        </>
+       
+      );
+    }
+export default Promotion
