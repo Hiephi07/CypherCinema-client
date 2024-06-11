f7ea168b483ac26cb7b0b7b293491593af777788
duc2807 <nguyentrungduc2807@gmail.com>
2024-06-11 21:16:58 +0700

    feat[header, footer]

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ import Header from "./_components/client/Header/Header";
 import HomePage from "./pages/client/home-pages/HomePage";
 import LoginPage from "./pages/client/login/LoginPage";
 import Footer from "./_components/client/Footer/Footer";
+import Event from "./_components/client/Event/Event";
 
 
 function App() {
   return (
     <>
     <Header />
+    <Event />
       <Routes>
         <Route path="/">
           <Route index element={<HomePage />} />
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
+import ThemeProvider from "./_context/ThemeProvider.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
