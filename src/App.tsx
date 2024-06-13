f546865a38b18bdf291c2a0fe24c95b49b236a82
Xuan Tho <tholxph31196@fpt.edu.vn>
2024-06-13 18:45:38 +0700

    fix[slide]

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.scss";
 import "../public/assets/fonts/fonts.scss";
-// import HomePage from "./pages/client/home-pages/HomePage";
-// import LoginPage from "./pages/client/login/LoginPage";
 import Header from "./_components/client/Header/Header";
 import HomePage from "./pages/client/home-pages/HomePage";
 import LoginPage from "./pages/client/login/LoginPage";
 import Footer from "./_components/client/Footer/Footer";
 import Event from "./_components/client/Event/Event";
 
-
 function App() {
   return (
     <>
-    <Header />
-    <Event />
+      <Header />
+      <Event />
       <Routes>
         <Route path="/">
           <Route index element={<HomePage />} />
--- src/pages/client/home-pages/HomePage.tsx
+++ src/pages/client/home-pages/HomePage.tsx
@@ -10,8 +10,6 @@ const HomePage = () => {
       <ModalMovieProvider>
         <UpComingMovies />
       </ModalMovieProvider>
-      {/* <Header /> */}
-      <UpComingMovies />
     </>
   );
 };
